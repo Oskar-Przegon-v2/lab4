3e81c87d2ae87a74860ebd3b4198dfa8fdbd3c79
„Oskar” <dsw52774@student.dsw.edu.pl>
2024-06-09 15:25:18 +0200

    dodano obsluge errorow

--- lab4.sh
+++ lab4.sh
@@ -5,7 +5,8 @@ show_help() {
     echo "--date, -d        Pokazuje dzisiejsza date"
     echo "--logs, -l [N]    Tworzy N plikow log (domyslnie: 100)"
     echo "--help, -h        Pokazuje pomoc"
-    echo "--init            Clone the repository and set PATH"
+    echo "--init            Klonuje repozytorium i ustawia PATH"
+    echo "--error, -e [N]   Tworzy N plikow error (domyslnie: 100)"
 
 }
 
@@ -25,6 +26,18 @@ init_repo() {
     export PATH=$PATH:$(pwd)/cloned_repo
 }
 
+create_errors() {
+    num_files=${1:-100}
+    mkdir -p errorx
+    for i in $(seq 1 $num_files)
+    do
+        filename="errorx/error$i.txt"
+        echo "Filename: $filename" > $filename
+        echo "Script: $0" >> $filename
+        echo "Date: $(date)" >> $filename
+    done
+}
+
 case "$1" in
     --date|-d)
         date
@@ -37,6 +50,12 @@ case "$1" in
         ;;
     --init)
         init_repo
+        ;;
+    --error|-e)
+        create_errors $2
+        ;;
+    *)
+        show_help
         ;;
 esac
 
